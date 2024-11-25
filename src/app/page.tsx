0c5101978cd34d1b4e30f63e5ab1d55c556df614
Shubhansh Sharma <shubhanshsharma030604@gmail.com>
2024-11-26 00:23:23 +0530

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,58 @@
+"use client";
 import Image from "next/image";
+import homeImage from "./Assets/Home_Image.png";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const homeImageRef = useRef(null);
+  const leftRect = useRef(null);
+  const rightRect = useRef(null);
+  const LandingButton = useRef(null);
+  const huntersHeading = useRef(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useEffect(() => {
+
+    //image reveal animation
+    gsap.to(leftRect.current, { x: '-50vw', duration: 3, delay: 0, ease: 'power4.out' });
+    gsap.to(rightRect.current, { x: '50vw', duration: 3, delay: 0, ease: 'power4.out' });
+    // gsap.to(leftRect.current, { scale: 0, duration: 0, delay: 2.5,});
+    // gsap.to(rightRect.current, { scale: 0 , duration: 0, delay: 2.5,});
+
+    //image animation
+    let homeImageTl = gsap.timeline();
+    homeImageTl.fromTo(homeImageRef.current,{scale: 0.7, opacity: 1,}, { opacity: 1 ,scale: 1, duration: 1.3, delay: 0, ease: 'power1.in' });
+    homeImageTl.fromTo(homeImageRef.current,{y: 100}, {y:0, duration: 1, ease: 'power4.out'});
+    gsap.to(homeImageRef.current,{boxShadow:'1px 22px 50px 0px rgba(240,240,240,1)',delay: 1.3, duration: 0.6, ease: 'power4.out'})
+    gsap.to(homeImageRef.current,{boxShadow: '1px 22px 0px 0px rgba(240,240,240,0)',delay: 1.7,  duration: 1, ease: 'power4.out'})
+
+    // button animation
+    gsap.fromTo(LandingButton.current,{x: '-50vw'} , { x: 0, duration: 1, delay: 2, ease: 'power3.out' });
+
+
+    // hunters heading animation
+    gsap.fromTo(huntersHeading.current,{x: 100, opacity: 0} , { x: 0, opacity: 1, duration: 1, delay: 2.6, ease: 'power3.out' });
+    
+
+  },[])
+
+  return (
+    <div style={{ overflowX: 'hidden' }} className=" text-neutral-200 overflow-x-hidden flex flex-col items-center w-screen min-h-screen px-14 bg-neutral-950 font-[family-name:var(--font-geist-mono)]">
+      
+      <Image ref={homeImageRef} src={homeImage} alt="homeImage" className=" w-full mt-[2em] rounded-[60px]"/>
+      <div ref={leftRect} className=" absolute left-0 top-0 w-1/2 h-screen bg-neutral-950 "></div>
+      <div ref={rightRect} className=" absolute right-0 top-0 w-1/2 h-screen bg-neutral-950 "></div>
+
+      <button ref={LandingButton} className="mt-16 text-black bg-neutral-200 rounded-full self-start px-10 py-3 font-light text-sm">
+        Join techhunterssss community 
+      </button>
+
+      {/* TechHunterss heading */}
+      <span ref={huntersHeading} className=" self-start text-[90px] font-black -mt-1">
+        techhunterssss
+      </span>
+
     </div>
   );
 }
